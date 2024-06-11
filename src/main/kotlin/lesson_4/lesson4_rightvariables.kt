9116524a9893d7b09dbb915bc77729778c634278
Nikita_Rudometov <giwamr@gmail.com>
2024-06-11 19:49:23 +0700

    KS-1-4-rightVariables

---
+++ src/main/kotlin/lesson_4/lesson4_rightvariables.kt
@@ -0,0 +1,20 @@
+package org.example.lesson_4
+
+fun main (){
+
+    val age : Byte = 27
+    val length : Long = 40_868_600_000
+    val partOfDay : Double = 0.075
+    val seconds : Short = 6480
+    val partOfYear : Double = 2.0547945205479453E-4
+    val apogee : Int = 327000
+
+    print("""
+        age: $age
+        length: $length
+        partOfDay: $partOfDay
+        seconds: $seconds
+        partOfYear: $partOfYear
+        apogee: $apogee
+    """.trimIndent())
+}
