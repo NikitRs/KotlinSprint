f82d872dd3bb278a5894e12764c2f632ba4ffe6a
Nikita_Rudometov <giwamr@gmail.com>
2024-06-13 15:55:58 +0700

    KS-1-5-Удалил лишний файл и изменил решения задачи

--- src/main/kotlin/lesson_5/lesson5_seconds.kt
+++ src/main/kotlin/lesson_5/lesson5_seconds.kt
@@ -2,15 +2,13 @@ package org.example.lesson_5
 
 fun main (){
 
-    val mainSeconds : Short = 6480
+    val mainSeconds = 6480
     val secondsInHour = 3600
     val minuteInHour = 60
-    val hours = (mainSeconds / secondsInHour)
-    val fullHours = hours
-    val minute = (mainSeconds % secondsInHour)
-    val fullMinutes = (minute / minuteInHour)
-    val fullSeconds = (minute % minuteInHour)
-    val result = String.format("Проведенное время в космосе: %02d:%02d:%02d", fullHours, fullMinutes, fullSeconds)
+    val hours = mainSeconds / secondsInHour
+    val minutes = (mainSeconds % secondsInHour) / minuteInHour
+    val seconds = mainSeconds % minuteInHour
+    val result = String.format("Проведенное время в космосе: %02d:%02d:%02d", hours, minutes, seconds)
 
     println(result)
 
