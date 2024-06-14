101235208b50a8248474af35f9cc3da194d0e80f
Nikita_Rudometov <giwamr@gmail.com>
2024-06-14 22:13:40 +0700

    KS-1-5-Объявлены константы

--- src/main/kotlin/lesson_5/lesson5_seconds.kt
+++ src/main/kotlin/lesson_5/lesson5_seconds.kt
@@ -1,13 +1,14 @@
 package org.example.lesson_5
 
+    const val MINUTE_IN_HOUR = 60
+    const val SECOND_IN_HOUR = 3600
+
 fun main (){
 
     val mainSeconds = 6480
-    val secondsInHour = 3600
-    val minuteInHour = 60
-    val hours = mainSeconds / secondsInHour
-    val minutes = (mainSeconds % secondsInHour) / minuteInHour
-    val seconds = mainSeconds % minuteInHour
+    val hours = mainSeconds / SECOND_IN_HOUR
+    val minutes = (mainSeconds % SECOND_IN_HOUR) / MINUTE_IN_HOUR
+    val seconds = mainSeconds % MINUTE_IN_HOUR
     val result = String.format("Проведенное время в космосе: %02d:%02d:%02d", hours, minutes, seconds)
 
     println(result)
