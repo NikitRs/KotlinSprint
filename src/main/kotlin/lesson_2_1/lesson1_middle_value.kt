edab219ee1c3a559af7bfdb784c140f53b0dcc20
Nikita_Rudometov <giwamr@gmail.com>
2024-06-14 22:41:08 +0700

    KS-2-1-Решено первое задание

---
+++ src/main/kotlin/lesson_2_1/lesson1_middle_value.kt
@@ -0,0 +1,15 @@
+package lesson_2_1
+
+fun main(){
+
+    val children = 4f
+    val ratingOne = 3
+    val ratingTwo = 4
+    val ratingThree = 3
+    val ratingFour = 5
+
+    val middleValue = (ratingOne + ratingTwo + ratingThree + ratingFour) / children
+
+    println("Средний балл учеников составляет: $middleValue")
+
+}
