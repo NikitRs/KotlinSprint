8a739f62f952d20ab15301edb4f6a27e7e771805
Nikita_Rudometov <giwamr@gmail.com>
2024-06-14 23:03:47 +0700

    KS-2-2-Решено второе задание второго модуля

---
+++ src/main/kotlin/lesson_2_2/lesson2_salary.kt
@@ -0,0 +1,17 @@
+package lesson_2_2
+
+fun main (){
+
+    val mainEmployee = 50
+    val mainEmployeeSalary = 50000
+    val trainee = 30
+    val traineeSalary = 30000
+
+    val expenseMainSalary = (mainEmployee * mainEmployeeSalary)
+    val generalExpenseSalary = (expenseMainSalary + (trainee * traineeSalary))
+    val middleSalary = (generalExpenseSalary / (mainEmployee + trainee))
+
+    println("Расходы на зарплату основных сотрудников составляют: $expenseMainSalary$")
+    println("Общий расход на зарплату основных сотрудников и стажеров составляет: $generalExpenseSalary$")
+    println("Средняя зарплата сотрудника в организации составляет: $middleSalary")
+}
