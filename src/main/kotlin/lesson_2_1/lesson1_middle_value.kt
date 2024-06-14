22c663f9be6a78414defcfe73ef564ceb721977d
Nikita_Rudometov <giwamr@gmail.com>
2024-06-14 22:53:19 +0700

    KS-2-1-Решено первое задание

--- src/main/kotlin/lesson_2_1/lesson1_middle_value.kt
+++ src/main/kotlin/lesson_2_1/lesson1_middle_value.kt
@@ -2,14 +2,14 @@ package lesson_2_1
 
 fun main(){
 
-    val children = 4f
+    val children = 4
     val ratingOne = 3
     val ratingTwo = 4
     val ratingThree = 3
     val ratingFour = 5
 
-    val middleValue = (ratingOne + ratingTwo + ratingThree + ratingFour) / children
+    val middleValue = (ratingOne + ratingTwo + ratingThree + ratingFour).toDouble() / children
+    val result = "Средний арифметический балл в классе равен: $middleValue"
 
-    println("Средний балл учеников составляет: $middleValue")
-
+    println(result)
 }
