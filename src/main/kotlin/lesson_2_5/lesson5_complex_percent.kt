84eb838e835f68dbb824d95c0e80b52e727c6652
Nikita_Rudometov <giwamr@gmail.com>
2024-06-15 22:42:35 +0700

    KS-2-5-Избавились от "магических" чисел

--- src/main/kotlin/lesson_2_5/lesson5_complex_percent.kt
+++ src/main/kotlin/lesson_2_5/lesson5_complex_percent.kt
@@ -2,12 +2,14 @@ package lesson_2_5
 
 import kotlin.math.pow
 
+const val PERCENT_DIVISOR = 100
+
 fun main(){
 
     val years = 20
     val moneys = 70_000
     val timesCompounded = 1
-    val percent = 16.7 / 100
+    val percent = 16.7 / PERCENT_DIVISOR
 
     val endSum = moneys * (1 + percent / timesCompounded).pow(timesCompounded * years)
 
