a5a7e78d652c3ff3a7331574ad586eb0ff427e23
Nikita_Rudometov <giwamr@gmail.com>
2024-06-16 14:05:26 +0700

    KS-2-3-Избавились от условий

--- src/main/kotlin/lesson_2_3/lesson3_train_arrival.kt
+++ src/main/kotlin/lesson_2_3/lesson3_train_arrival.kt
@@ -2,22 +2,17 @@ package lesson_2_3
 
 const val MINUTE_IN_HOUR = 60
 
-fun main (){
+fun main() {
 
     val trainHourStart = 9
     val trainMinutesStart = 39
     val trainGeneralMinutesWay = 457
 
-    val trainHourWay = (trainGeneralMinutesWay / MINUTE_IN_HOUR)
-    val trainMinutesWay = (trainGeneralMinutesWay % MINUTE_IN_HOUR)
 
-    var trainHourArrival = trainHourStart + trainHourWay
-    var trainMinutesArrival = trainMinutesStart + trainMinutesWay
-
-    if (trainMinutesArrival >= MINUTE_IN_HOUR){
-        trainMinutesArrival -= MINUTE_IN_HOUR
-        trainHourArrival++
-    }
+    val startTotalMinutes = trainHourStart * MINUTE_IN_HOUR + trainMinutesStart
+    val arrivalTotalMinutes = startTotalMinutes + trainGeneralMinutesWay
+    val trainHourArrival = arrivalTotalMinutes / MINUTE_IN_HOUR
+    val trainMinutesArrival = arrivalTotalMinutes % MINUTE_IN_HOUR
 
     println("Поезд прибудет в %02d:%02d".format(trainHourArrival, trainMinutesArrival))
 }
