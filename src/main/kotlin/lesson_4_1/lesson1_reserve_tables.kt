da6e9efd6a4fb4f9aabf2727e16f9dbb17fd2b38
Nikita_Rudometov <giwamr@gmail.com>
2024-06-16 18:29:25 +0700

    KS-4-1-Разработано решение первой задачи четвертого блока

---
+++ src/main/kotlin/lesson_4_1/lesson1_reserve_tables.kt
@@ -0,0 +1,14 @@
+package lesson_4_1
+
+fun main () {
+
+    val fullTables = 13
+    val reserveTablesToday = 13
+    val reserveTablesTomorrow = 9
+
+    val reserveToday = reserveTablesToday < fullTables
+    val reserveTomorrow = reserveTablesTomorrow < fullTables
+
+    println("Доступность столиков на сегодня: $reserveToday \nДоступность столиков на завтра: $reserveTomorrow ")
+
+}
