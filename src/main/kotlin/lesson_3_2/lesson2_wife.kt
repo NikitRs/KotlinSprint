5717c7a30ae342acfd2150d2b46ab50dfa5c2ee8
Nikita_Rudometov <giwamr@gmail.com>
2024-06-16 14:24:48 +0700

    KS-3-2-Разработано решение второй задачи третьего блока

---
+++ src/main/kotlin/lesson_3_2/lesson2_wife.kt
@@ -0,0 +1,12 @@
+package lesson_3_2
+
+fun main(){
+
+    val presentName = "Сидорова Татьяна Сергеевна"
+    val presentAge = 22
+    val pastName = "Андреева Татьяна Сергеевна"
+    val pastAge = 20
+
+    println("В возрасте $pastAge лет ваше ФИО было: $pastName")
+    println("В данный момент вам $presentAge года и ваше ФИО: $presentName")
+}
