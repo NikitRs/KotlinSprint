73813cf4847284abde05a8e28dc1b46875fea300
Nikita_Rudometov <giwamr@gmail.com>
2024-06-19 18:32:20 +0700

    KS-4-4-Изменили решение задачи

--- src/main/kotlin/lesson_4_4/lesson4_sport_exercise.kt
+++ src/main/kotlin/lesson_4_4/lesson4_sport_exercise.kt
@@ -3,32 +3,20 @@ package lesson_4_4
 fun main() {
 
     val trainingDay = 6
-    val armTraining: Boolean
-    val pressTraining: Boolean
-    val legTraining: Boolean
-    val backTraining: Boolean
+    val evenDay = trainingDay % 2 == 0
+    val armTraining: Boolean = !evenDay
+    val pressTraining: Boolean = !evenDay
+    val legTraining: Boolean = evenDay
+    val backTraining: Boolean = evenDay
 
-    val evenDay = trainingDay % 2
-
-    if (evenDay == 0) {
-        armTraining = false
-        pressTraining = false
-        legTraining = true
-        backTraining = true
-    }
-    else {
-        armTraining = true
-        pressTraining = true
-        legTraining = false
-        backTraining = false
-    }
-
-    println("""
+    println(
+        """
         Упражнение для рук: $armTraining
         Упражнение для ног: $legTraining
         Упражнение для спины: $backTraining
         Упражнение для пресса: $pressTraining
-    """.trimIndent())
+    """.trimIndent()
+    )
 
 
 }
